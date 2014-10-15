be2e161fcd99d5ab41a6ec06c532a4b3c1202616
Sofien Moalla <sofien.moalla@e-erp-sys.com>
2014-10-15 16:02:06 +0100

    Product Report Menu

---
+++ product_report_menu/__openerp__.py
@@ -0,0 +1,26 @@
+{
+    'name' : 'Product Report Menu',
+    'version' : '1.0',
+    'author': 'ERP SYSTEMS',
+    'website': '',    
+    'summary': 'Product Report Menu',
+    'description': """
+    
+Features:
+==========
+
+* Adds a menu item "Report" for wizards .
+ 
+    """,
+    'category': 'Sales Management',
+    'sequence': 25,
+    'website': 'http://www.e-erp-sys.com',
+    'depends' : ['base'],
+    'data' : [
+              'product_menu_view.xml',
+              ],
+    'test' : [],
+    'auto_install': False,
+    'application': True,
+    'installable': True,
+}
